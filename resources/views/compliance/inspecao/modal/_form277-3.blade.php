f6f4674540b8797c3180f7d68ff8907896c77b46
abiliod <abiliobonito@gmail.com>
2020-12-18 00:07:09 -0300

    18/12/20 - Correção de entrada de dados no modal;

--- resources/views/compliance/inspecao/modal/_form277-3.blade.php
+++ resources/views/compliance/inspecao/modal/_form277-3.blade.php
@@ -1,14 +1,24 @@
 @if($count== 0)
-    <div id="aprimoramento">
+    @if($registro->tem_distribuicao == 'Não tem distribuição')
+        <div id="aprimoramento">
         <span class="lever rigth">
-                Não há registro na base de dados para verificação. É possível que a unidade não executa essa tarefa, verifique a existência de SmartPhone na unidade.
+                A unidade não executa essa tarefa.
         </span>
-    </div>
-    <div id="historico">  </div>
-    <div id="historico1"></div>
+        </div>
+        <div id="historico"> </div>
+        <div id="historico1"></div>
+    @else
+        <div id="aprimoramento">
+        <span class="lever rigth">
+                Não há registro na base de dados para verificação. verifique a existência de SmartPhone na unidade.
+        </span>
+        </div>
+        <div id="historico">  </div>
+        <div id="historico1"></div>
+    @endif
 @endif
 
-@if($qtd_falhas == 0)
+@if (($qtd_falhas == 0) && ($registro->tem_distribuicao <> 'Não tem distribuição'))
     <div id="aprimoramento">
         <span class="lever rigth">
                 Foram avaliados {{$amostra}} Imagens de Objetos, relativas ao período de {{substr($dtini,8,2)}}/{{substr($dtini,5,2)}}/{{substr($dtini,0,4)}} até {{substr($dtfim,8,2)}}/{{substr($dtfim,5,2)}}/{{substr($dtfim,0,4)}}, que representava 100 por cento, do total da amostra para o período mencionado, sendo que em 100 por cento, não constataram inconsistências.
@@ -16,9 +26,7 @@
     </div>
     <div id="historico">  </div>
     <div id="historico1"></div>
-@else
-
-
+@elseif ($res >  0)
     <div id="aprimoramento">
         <span class="lever rigth">
             Em análise, por amostragem, às anotações registradas nas Imagens dos comprovantes
@@ -26,7 +34,6 @@
               inconsistências relacionadas aos procedimentos de entrega:
         </span>
     </div>
-
     <div id="historico">
         <table class="highlight">
             <thead>
@@ -36,21 +43,16 @@
                 <th>Endereço LOEC</th>
                 <th>Local Baixa 1ª Tent.</th>
                 <th>Falha detectada</th>
-
             </tr>
             </thead>
             <tbody>
             @foreach($res as $dado)
                 <tr>
                     <td><a href="http://wsmobile.correiosnet.int/ComprovanteSRO/entrega?ITEMCODE={{ $dado->objeto }}" target="_blank">{{ $dado->objeto }}</a></td>
-
                     <td>{{ date( 'd/m/Y' , strtotime($dado->data))}}</td>
                     <td>{{$dado->enderecoPostagem}}</td>
                     <td>{{$dado->localBaixa1tentativa}}</td>
-
                     <td>{{$dado->falhaDetectada}}</td>
-
-
                 </tr>
             @endforeach
             </tbody>
