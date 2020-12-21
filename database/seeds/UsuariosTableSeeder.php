cd29c535a9db346cc6c60c453d2aee455a90399d
abiliod <abiliobonito@gmail.com>
2020-12-21 20:06:31 -0300

    21/12/20 - Ajuste de inclusão de campos para permissões e papeis de usuários;

--- database/seeds/UsuariosTableSeeder.php
+++ database/seeds/UsuariosTableSeeder.php
@@ -1,8 +1,10 @@
 <?php
 use Illuminate\Database\Seeder;
 use App\User;
+use App\Papel_user;
 
 class UsuariosTableSeeder extends Seeder{
+// php artisan db:seed --class=UsuariosTableSeeder
     /**
     * @return void
     */
@@ -10,50 +12,47 @@ class UsuariosTableSeeder extends Seeder{
 
         if(User::where('email','=','admin@sgiweb.com')->count()){
             $usuario = User::where('email','=','admin@sgiweb.com')->first();
-            $usuario->name = "Masterkey-Abilio";
+            $usuario->id	 = '231';
+            $usuario->se	 = '16';
+            $usuario->name	 = 'ABILIO DIAS FERREIRA';
             $usuario->document	 = '83288082';
-            $usuario->businessUnit	 = '434448';
-            $usuario->email = "admin@sgiweb.com";
-            $usuario->password = bcrypt("12345678");
+            $usuario->businessUnit	 = '434446';
+            $usuario->coordenacao	 = 'BSB';
+            $usuario->funcao	 = 'INSPETOR REGIONAL - G4';
+            $usuario->localizacao	 = 'GOIÂNIA';
+            $usuario->telefone_ect	 = '';
+            $usuario->telefone_pessoal	 = '';
+            $usuario->email	 = 'admin@sgiweb.com';
+            $usuario->password = bcrypt('83288082');
             $usuario->save();
         }else{
             $usuario = new User();
-            $usuario->name = "Masterkey-Abilio";
-            $usuario->email = "admin@sgiweb.com";
+            $usuario->id	 = '231';
+            $usuario->se	 = '16';
+            $usuario->name	 = 'ABILIO DIAS FERREIRA';
             $usuario->document	 = '83288082';
             $usuario->businessUnit	 = '434448';
-            $usuario->password = bcrypt("12345678");
+            $usuario->coordenacao	 = 'BSB';
+            $usuario->funcao	 = 'INSPETOR REGIONAL - G4';
+            $usuario->localizacao	 = 'GOIÂNIA';
+            $usuario->telefone_ect	 = '';
+            $usuario->telefone_pessoal	 = '';
+            $usuario->email	 = 'admin@sgiweb.com';
+            $usuario->password = bcrypt('83288082');
             $usuario->save();
         }
+        Papel_user::UpdateOrCreate([
+            'user_id'=>$usuario->id ,
+            'papel_id'=>'1'
+        ]);
 
+        echo '\n Usuario Master criado com sucesso!'
+        ,'\n email = admin@sgiweb.com'
+        ,'\n password = 12345678';
 
-        if(User::where('email','=','admin@compliance.com')->count()){
-            $usuario = User::where('email','=','admin@compliance.com')->first();
-            $usuario->name = "Masterkey-MarcosV";
-            $usuario->email = "admin@compliance.com";
-            $usuario->document	 = '89056582';
-            $usuario->businessUnit	 = '434063';
-            $usuario->password = bcrypt("12345678");
-            $usuario->save();
-        }else{
-            $usuario = new User();
-            $usuario->name = "Masterkey-MarcosV";
-            $usuario->email = "admin@compliance.com";
-            $usuario->document	 = '89056582';
-            $usuario->businessUnit	 = '434063';
-            $usuario->password = bcrypt("12345678");
-            $usuario->save();
-        }
-
-
-
-        echo "\n Usuario Master criado com sucesso!"
-        ,"\n email = admin@compliance.com"
-        ,"\n password = 12345678";
-
-        echo "\n Demais Usuários criados com sucesso!"
-        ,"\n email = >seuemail>@correios.com.br"
-        ,"\n password = MATRICULA-> 99999999";
+        echo '\r\n Demais Usuários criados com sucesso!'
+        ,'\n email = >seuemail>@correios.com.br'
+        ,'\n password = MATRICULA-> 99999999';
 
     }
 }
