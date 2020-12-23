74f20283b7f9bac708633be52b3da96b154cbfc1
abiliod <abiliobonito@gmail.com>
2020-12-22 22:48:22 -0300

    22/12/20 - Ajuste inclusão de funcionalidade add tipo de unidade;

--- resources/views/compliance/tipounidades/adicionar.blade.php
+++ resources/views/compliance/tipounidades/adicionar.blade.php
@@ -1,37 +1,24 @@
 @extends('layouts._sgiweb.app')
-
 @section('content')
-<div class="container">
-	<h2 class="center">Adição de Grupo de Verificação</h2>
-	<div class="row">
-	 	<nav>
-         <div class="nav-wrapper green">
-		      	<div class="col s12">
-			        <a href="{{ route('home')}}" class="breadcrumb">Início</a>
-			        <a href="{{route('compliance.grupoVerificacao')}}" class="breadcrumb">Grupos de Verificação</a>
-			        <a class="breadcrumb">Adição de Grupo de Verificação</a>
-		      	</div>
-            </div>
-
-          </nav>
-    </div>
-    @if($errors->any())
-        <div class="row red">
-        @foreach($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-        </div>
-    @endif
-	<div class="row">
-		<form action="{{ route('compliance.grupoVerificacao.salvar') }}" method="post">
-		    @CSRF
-            @include('compliance.grupoVerificacao._form')
-            @can('tipoUnidade_adicionar')
-                <div class="input-field col s12">
-                    <button class="btn blue">Adicionar</button>
+    <div class="container">
+        <h2 class="center">Adicionar Tipo de Unidades</h2>
+        <div class="row">
+            <nav>
+                <div class="nav-wrapper green">
+                    <div class="col s12">
+                        <a href="{{ route('home')}}" class="breadcrumb">Início</a>
+                        <a href="#" class="breadcrumb">Lista de Unidades</a>
+                        <a class="breadcrumb">Adicionar Unidades</a>
+                    </div>
                 </div>
-            @endcan
-		</form>
-	</div>
-</div>
+            </nav>
+        </div>
+        <div class="row">
+            <form action="{{ route('compliance.tipounidades.salvar') }}" method="post">
+                {{csrf_field()}}
+                @include('compliance.tipounidades._form')
+                <button class="btn blue">Adicionar</button>
+            </form>
+        </div>
+    </div>
 @endsection
