38bce90d0b1e589cc24bd217893efa201ed91d08
abiliod <abiliobonito@gmail.com>
2020-12-22 22:52:03 -0300

    22/12/20 - Ajuste de apontamento de itens de relatos inspeção;

--- resources/views/compliance/relatos/editar.blade.php
+++ resources/views/compliance/relatos/editar.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts._sgiweb.app')
 @section('content')
 <div class="container">
-	<h2 class="center">Editar Itens de Verificação</h2>
+	<h2 class="center">Editar Itens de Inspeções</h2>
 	<div class="row">
 	 	<nav>
 		    <div class="nav-wrapper green">
 		      	<div class="col s12">
 			        <a href="{{ route('home')}}" class="breadcrumb">Início</a>
 			        <a href="{{route('compliance.relatos')}}" class="breadcrumb">Itens de Verificação</a>
-			        <a class="breadcrumb">Editar Itens de Verificação</a>
+			        <a class="breadcrumb">Editar Itens de Inspeções</a>
 		      	</div>
 		    </div>
 	  	</nav>
