b54b85b514b575af7213ebaf07f4a0399fe8892a
abiliod <abiliobonito@gmail.com>
2020-12-23 22:11:11 -0300

    23/12/20 - ajuste final para importar a função Proter;

--- resources/views/compliance/importacoes/proter.blade.php
+++ resources/views/compliance/importacoes/proter.blade.php
@@ -18,17 +18,33 @@
             <div class="card #0d47a1 blue darken-4">
                 <div class="card-content white-text">
                     <span class="card-title">Sistema Proter</span>
-                    <p>Grupo de Verificação 270, Função Prevenção de Perdas   </p>
-                    <p>Assunto: Proteção de Receitas. PROTER | 270-2-FINANCEIRO-Proter_ProtecaoReceita.xlsx</p>
-                    <p><b>Modo Truncate</b> Talvez necessita ajuste para aderencia Nacional</p>
-                    <thead>
-                        <tr>
-                            <th>Abrir o Arquivo excluir a primeira linha | </th>
-                            <th>Salvar a Planilha como Pasta de Trabalho Excel  formato XLSX| </th>
-                            <th>O arquivo original traz informações da coluna MCU e Nome da unidade em colunas divergentes | </th>
-                            <th>Foi ajustado durante o processo de importação por gentileza verificar as colunas e certificar das informações conforme planilha modelo na pasta.</th>
-                        </tr>
-                    </thead>
+                    <p>Função Prevenção de Perdas.</p>
+                    <p>Assunto: Proteção de Receitas. PROTER</p>
+                    <p>Item: 270-2 - FINANCEIRO </p>
+                    <p>
+                        <input id ="exibe" type="checkbox" class="filled-in col s1" onclick="Mudarestado('ajuda')"/>
+                        <label for="exibe">
+                            <span class="card-content orange-text">Exibir Ajuda do Item?</span>
+                        </label>
+                    </p>
+                    <div class="input-field"  id ="ajuda" style="display:none;">
+                        <i class="material-icons prefix">mode_edit</i>
+                        <textarea  id="ajuda" name="ajuda" class="materialize-textarea">
+                                Endereço:  http://app.correiosnet.int/SPNWEB/pages/inicio.jsf
+                                As informações são obtidas por meio: Sistema SPN->Itens Monitorados->Objeto Postal->Relatório Pendências Geral.
+                                As pendências apresentadas no PROTER referem-se ao período de Janeiro/2017 em diante.
+                                Efetuar as pesquisas a partir desta data. Ao gerar o arquivo (SELECIONE TODAS SEs).
+                                Fazer o download no formato (CSV). Importar os dados para uma Planilha formato xlsx.  Abrir o Arquivo excluir a primeira linha.
+                                Manteha sempre esse lay-out observe as palavras quando da importação: [TIPO DE PENDÊNCIA	STATUS DA PENDÊNCIA	DATA DA PENDÊNCIA	DIVERGÊNCIA PESO	DIVERGÊNCIA CEP	ORIGEM PENDÊNCIA	SE	TIPO DE UNIDADE	STO/MCU	NOME DA UNIDADE	STATUS DA UNIDADE	TIPO DE ATENDIMENTO	MATRÍCULA ATENDENTE	Nº DO OBJETO	DATA DA POSTAGEM	DATA DA ENTREGA	CÓDIGO DO SERVIÇO	CEP CONTABILIZADO (SARA)	CEP ENTREGA SRO	PESO TARIFADO FINANCEIRO	COMPRIMENTO FINANCEIRO	LARGURA FINANCEIRO	ALTURA FINANCEIRO	PESO CÚBICO FINANCEIRO	PESO REAL MECTRI	COMPRIMENTO MECTRI	LARGURA MECTRI	ALTURA MECTRI	PESO CÚBICO MECTRI	PESO TARIFADO MECTRI	VALOR TARIFADO FINANCEIRO	VALOR TARIFADO MECTRI	DIFERENÇA A RECOLHER	CNPJ DO CLIENTE	CONTRATO	CARTÃO POSTAGEM	NOME DO CLIENTE	QTD DUPLICIDADES	ÚLTIMA MANIFESTAÇÃO	[MCU Triagem	Centro	Peso	Volume	Altura	Largura	Comprimento	Data de leitura	Tipo do objeto	Cep destino	Tipo de indução	Número da máquina	Código da estação]
+                                Exclua os registros que na coluna TIPO DE PENDÊNICIA for igual ORD e SRO, salve a planilha e execute a importação.
+                                Sugestão de nome do Arquivo: 270-2-FINANCEIRO-Proter_ProtecaoReceita.xlsx.
+                                Frequencia: Semanal. (obs: Há possibilidade de uma pendência em anos anteriores ter sido regularizada recentemente.)
+                                Se o sistema der erro TIME-OUT, Informe esse erro ao Administrador do sistema.
+
+                        </textarea>
+                    </div>
+
+
                     <form action="{{ route('compliance.importacao.proter') }}" method="POST" name="importform"
                             enctype="multipart/form-data">
                         {{csrf_field()}}
